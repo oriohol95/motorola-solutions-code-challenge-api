444eea019ad20eb30edab123a53ffeb5d2e5e343
Oriol Colomer <ocolomer@Oriols-MacBook-Pro.local>
2024-01-18 12:19:37 -0600

    fix: update cors origin

--- api/middlewares/cors.js
+++ api/middlewares/cors.js
@@ -1,8 +1,7 @@
 import cors from 'cors'
 
 const ACCEPTED_ORIGINS = [
-  'http://localhost:3002',
-  '*'
+  'http://localhost:3000'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
