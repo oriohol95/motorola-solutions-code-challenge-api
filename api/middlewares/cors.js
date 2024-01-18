b3153853393358130f443e001d480f7a00f3535f
Oriol Colomer <ocolomer@Oriols-MacBook-Pro.local>
2024-01-18 12:26:18 -0600

    fix: add prod url to accepted origins

--- api/middlewares/cors.js
+++ api/middlewares/cors.js
@@ -1,7 +1,8 @@
 import cors from 'cors'
 
 const ACCEPTED_ORIGINS = [
-  'http://localhost:3000'
+  'http://localhost:3000',
+  'https://motorola-solutions-code-challenge-web.vercel.app'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
